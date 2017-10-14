ba585cee94f3afe33df48b3f22ad4662890213b3
Pablofero <pablofrldp@gmail.com>
2017-10-14 23:57:10 +0200

    added option for  same charge / different charge

--- sketch.js
+++ sketch.js
@@ -8,19 +8,23 @@ function draw() {
     let maxSize =sqrt(2*(size*size))/2;
     let v1 = createVector(200,height/2)
     let v2;
-    if(true){
+    if(!true){
         v2 = createVector(width-200,height/2)
      }else{
         v2 = createVector(mouseX,mouseY);
     }
     for (let i = size/2; i < width; i += size) {
         for (let j = size/2; j < height; j += size) {
+            
             let p = createVector(i,j);
             let f1 =-1/p.dist(v1);
             let f2 =-1/p.dist(v2);
             let final1 = p5.Vector.mult(p5.Vector.sub(p,v2).normalize(),f2);
             let finale2 = p5.Vector.mult(p5.Vector.sub(p,v1).normalize(),f1);
-            let force = p5.Vector.add(final1,finale2);
+            let force;
+
+            if(keyIsPressed )force = p5.Vector.add(final1,finale2);
+            else force = p5.Vector.sub(final1,finale2);
           arrow(i, j,size,maxSize, force);
             
         }
